529e8568ff9f60b04c04d55eb58e6100e4969a9d
Thin-Myat-Naing <52567884+Thin-Myat-Naing@users.noreply.github.com>
2022-02-23 15:03:08 +0630

    Delete Portolio Project directory

--- Portolio Project/script.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.querySelector(".navbar");
-const mainFn = () => {
-  if (window.pageYOffset >= navbarOffsetTop) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-  };
